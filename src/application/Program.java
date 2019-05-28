23e880bcb980750e1a02b0c052fde9613c484086
ommathias <mathias.ufg@gmail.com>
2019-05-28 16:06:01 -0300

    Primeira classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Position p = new Position(2,3);
+		
+		System.out.println(p);
 	}
 
 }
