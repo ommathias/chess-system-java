0812c67469f515b4f42be71eb76d49b7e878aff6
ommathias <mathias.ufg@gmail.com>
2019-05-29 14:09:11 -0300

    Começou implementar board e piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,13 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Position p = new Position(2,3);
-		
-		System.out.println(p);
+		Board board = new Board (8,8);
 	}
 
 }
