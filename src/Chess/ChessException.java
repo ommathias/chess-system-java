88612d0d990af36e3685352f44aa28e59dd9fcc6
ommathias <mathias.ufg@gmail.com>
2019-06-07 19:30:34 -0300

    ChessException and ChessPosition

---
+++ src/Chess/ChessException.java
@@ -0,0 +1,12 @@
+package Chess;
+
+public class ChessException extends RuntimeException{
+
+	public ChessException(String msg)
+	{
+		super(msg);
+	}
+	
+	private static final long serialVersionUID = 1L;
+
+}
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,12 +1,16 @@
 package boardgame;
 
 public class Board {
-	
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
-	
+
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1)
+		{
+			throw new BoardException("Erro ao criar tabuleiro (menor que 1x1)");
+		}
 		pieces = new Piece [rows][columns];
 		this.rows = rows;
 		this.columns = columns;
@@ -16,33 +20,56 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
 	public Piece piece (int row, int column)
 	{
+		if(!PositionExists(row, column))
+		{
+			throw new BoardException("Position do not exists!");
+		}
 		return pieces[row][column];
 	}
-	
+
 	public Piece piece (Position position)
-	
 	{
+		if(!PositionExists(position))
+		{
+			throw new BoardException("Position do not exists!");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece (Piece piece, Position position)
 	{
+		if(thereIsAPiece(position))
+		{
+			throw new BoardException("Theres already a piace on position " + position);
+		}
 		pieces [position.getRow()] [position.getColumn()] = piece;
 		piece.position = position;
+	}
+
+	public boolean PositionExists(int row, int column)
+	{
+		return row >= 0 && row < rows && column >= 0 && column <= columns;
+		
+	}
+	
+	public boolean PositionExists(Position position)
+	{
+		return PositionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position)
+	{
+		if(!PositionExists(position))
+		{
+			throw new BoardException("Position do not exists!");
+		}
+		return piece(position) != null;
 	}
 
 }
